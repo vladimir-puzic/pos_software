c6c34d1bad33e443be0bcf9c2ebb6c4b4de2ac84
zapu815225 <puza.freestyler@gmail.com>
2024-05-12 00:09:49 +0200

    feat: added create_customer() function

--- people.py
+++ people.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date, timedelta
 from random import randint, choice
 
 class Person:
@@ -24,6 +24,10 @@ class Employee(Person):
     
     def __str__(self):
         return f'{self._employee_id}: {self._f_name} {self._l_name} ({self._gender}) Phone number: {self._phone_no}'
+
+def random_date(start, end):
+    delta = end - start
+    
 
 def create_employee():
     genders = ['Female', 'Male']
@@ -53,6 +57,38 @@ def create_employee():
     employee_id = (f'{f_name[0].lower()}{l_name[0].lower()}{randint(000000,999999)}')
     return [f_name, l_name, gender, phone_no, employee_id]
 
+def create_customer():
+    genders = ['Female', 'Male']
+    gender = choice(genders)
+
+    if gender == 'Female':
+        names = []
+        with open('female_names.txt', 'r') as file:
+           for name in file:
+               names.append(name.strip())
+        f_name = choice(names)
+    elif gender == 'Male':
+        names = []
+        with open('male_names.txt', 'r') as file:
+           for name in file:
+               names.append(name.strip())
+        f_name = choice(names)
+
+    l_names = []
+    with open('last_names.txt', 'r') as file:
+        for name in file:
+            names.append(name.strip())
+    l_name = choice(names)
+
+    range_lenght = date.today() - date(1900, 1, 1)
+    total_days = range_lenght.days
+    dob = date(1900, 1, 1) + timedelta(days=choice(range(total_days)))
+
+    id = int(f'{randint(000000,999999):06d}')
+    
+    return [f_name, l_name, gender, dob, id]
+
+
 if __name__ == '__main__':
     cust1 = Customer('Nikolina', 'Zubac', 'Female', datetime(1996, 4, 1), 558)
     print (cust1)
@@ -61,3 +97,6 @@ if __name__ == '__main__':
     f_name, l_name, gender, phone_no, employee_id = create_employee()
     emp2 = Employee(f_name, l_name, gender, phone_no, employee_id)
     print (emp2)
+    f_name, l_name, gender, dob, id = create_customer()
+    cust2 = Customer(f_name, l_name, gender, dob, id)
+    print (cust2)
